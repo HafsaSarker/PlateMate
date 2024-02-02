4d6c270d2bd73aae1d2d9d4ef42f719c0be34d1a
Zaara Sarker <hafsahaqs@gmail.com>
2024-02-02 12:23:22 -0500

    config ts server and connect to db

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import { connectDB } from "../db/connect";
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const boot = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    server.listen(8080, () => {
+      console.log("Server running on http://localhost:8080/");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+boot();
