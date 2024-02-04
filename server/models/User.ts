0334ad9ee71c79f06ab48b07e651a05d7eee4fce
xchen <xchen601@gmail.com>
2024-02-03 21:44:12 -0500

    move models to models folder

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,1 +1,45 @@
-// create user model and schema here
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    authentication: {
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+        salt: {
+            type: String,
+            select: false,
+        },
+        sessionToken: {
+            type: String,
+            select: false,
+        },
+    },
+    profile: {
+        firstName: String,
+        lastName: String,
+        age: String,
+        foodPreferences: Array,
+        dislikedFoods: Array,
+        interests: Array,
+        profilePicture: String,
+        pricePreference: String, // "$", "$$", or "$$$"
+    }
+        
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const UserModel = mongoose.model("User", userSchema);
