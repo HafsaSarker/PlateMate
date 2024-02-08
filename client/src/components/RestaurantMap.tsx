e8d43860c63cd537844293fed919ce7e956ab3f6
xchen <xchen601@gmail.com>
2024-02-07 21:24:07 -0500

    add yelp api

--- client/src/components/RestaurantMap.tsx
+++ client/src/components/RestaurantMap.tsx
@@ -1,47 +1,37 @@
 import React ,  { useEffect, useState }from 'react';
 import { createRoot } from 'react-dom/client';
-import { APIProvider, Map, Marker, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { APIProvider, AdvancedMarker, Map, Marker, Pin, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
 import { config } from '../config';
-
-const RestaurantMarkers = ({location, setRestaurants}) => {
-  const map = useMap();
-
-  console.log(map)
-  const placesLib = useMapsLibrary('places');
-
-  useEffect(() => {
-    if (!placesLib) return;
-
-    const service  = new placesLib.PlacesService(map);
-    const request = {
-      location: new window.google.maps.LatLng(location.lat, location.lng),
-      radius: '2000', // Search within 500 meters
-      type: ['restaurant'], // Search for restaurants
-    };
-
-    service.nearbySearch(request, (results, status, pagination) => {
-      if (status !== "OK" || !results) return;
-
-      setRestaurants(results);
-      console.log(results); // You can remove this line once you confirm it's working
-    });
-
-
-  }, [map]); // This effect runs when the `map` object changes
-
-
-  return (
-    <>
-      <div>test</div>
-    </>
-  );
-}
 
 const RestaurantMap = () => {
   const [restaurants, setRestaurants] = useState([]);
-  const hunterLocation = { lat: 40.76785648078654, lng: -73.96447914218824 };
+  const currentLocation = { lat: 40.76785648078654, lng: -73.96447914218824 };
 
+  const yelpAPIKey = config.yelpAPIKey;
 
+  const fetchRestaurants = async () => {
+    try {
+      const response = await fetch(`/api/v3/businesses/search?term=restaurants` +
+      `&latitude=${currentLocation.lat}`+
+      `&longitude=${currentLocation.lng}`+
+      `&radius=500`+
+      `&sort_by=distance`+
+      `&limit=50`,
+        {
+          method: 'GET',
+      });
+      const data = await response.json();
+      console.log(data);
+      setRestaurants(data.businesses);
+      console.log(data.businesses)
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
   // Function to handle marker click
   const handleMarkerClick = (restaurantName) => {
     console.log(restaurantName);
@@ -50,17 +40,18 @@ const RestaurantMap = () => {
   return (
     <APIProvider apiKey={config.mapsAPIKey}>
       <div className='w-3/4 h-full p-11'>
-        <Map zoom={15} center={hunterLocation} mapId={config.mapID}>
+        <Map zoom={17} center={currentLocation} mapId={config.mapID}>
           {restaurants.map((restaurant, index) => (
             <Marker
               key={index}
-              position={restaurant.geometry.location}
+              position={{lat:restaurant.coordinates.latitude, lng:restaurant.coordinates.longitude}}
               onClick={() => handleMarkerClick(restaurant.name)}
             />
           ))}
-          <Marker position={hunterLocation} />
+          <AdvancedMarker position={currentLocation}>
+            <Pin background={'blue'} borderColor={'blue'} glyphColor={'lightblue'} scale={2}/>
+          </AdvancedMarker>
         </Map>
-        <RestaurantMarkers location={hunterLocation} setRestaurants={setRestaurants}/>
       </div>
     </APIProvider>
   );
