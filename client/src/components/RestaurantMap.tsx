f7b43fc520b214363983b2f047a5f8121cc9382b
Hafsa Sarker <hafsahaqs@gmail.com>
2024-02-10 22:04:44 -0500

    add middlewares

--- client/src/components/RestaurantMap.tsx
+++ client/src/components/RestaurantMap.tsx
@@ -1,7 +1,13 @@
-import React ,  { useEffect, useState, useCallback, useRef  }from 'react';
-import { APIProvider, AdvancedMarker, Map, Marker, Pin } from '@vis.gl/react-google-maps';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import {
+  APIProvider,
+  AdvancedMarker,
+  Map,
+  Marker,
+  Pin,
+} from '@vis.gl/react-google-maps';
 import { config } from '../config';
-import {debounce} from 'lodash';
+import { debounce } from 'lodash';
 import MapSearch from './MapSearch';
 
 interface MapEvent {
@@ -28,13 +34,18 @@ interface Restaurant {
   coordinates: {
     latitude: number;
     longitude: number;
-  },
-  categories: [{title: string;}]
+  };
+  categories: [{ title: string }];
 }
 
-const RestaurantMap:React.FC<RestaurantMapProps> = ({setClickedRestaurant}) => {
+const RestaurantMap: React.FC<RestaurantMapProps> = ({
+  setClickedRestaurant,
+}) => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-  const [currentLocation, setCurrentLocation] = useState({ lat: 40.76785648078654, lng: -73.96447914218824 });
+  const [currentLocation, setCurrentLocation] = useState({
+    lat: 40.76785648078654,
+    lng: -73.96447914218824,
+  });
 
   // hard coded for now
   const price = 1;
@@ -49,27 +60,32 @@ const RestaurantMap:React.FC<RestaurantMapProps> = ({setClickedRestaurant}) => {
   const fetchRestaurants = async () => {
     const { lat, lng } = mapCenterRef.current;
     try {
-      const response = await fetch(`/yelp-api/v3/businesses/search?term=restaurants` +
-      `&latitude=${lat}`+
-      `&longitude=${lng}`+
-      `&radius=40000`+
-      `&sort_by=distance`+
-      `&limit=50`+
-      `&price=${price}`,
+      const response = await fetch(
+        `/yelp-api/v3/businesses/search?term=restaurants` +
+          `&latitude=${lat}` +
+          `&longitude=${lng}` +
+          `&radius=40000` +
+          `&sort_by=distance` +
+          `&limit=50` +
+          `&price=${price}`,
         {
           method: 'GET',
-      });
+        },
+      );
       const data = await response.json();
       setRestaurants(data.businesses);
     } catch (error) {
       console.error('Error:', error);
     }
-  }
+  };
 
   // debounced api call so we don't make a request on every map move, updates after .4 second
-  const debouncedFetchRestaurants = useCallback(debounce(() => {
-    fetchRestaurants();
-  }, 400), []);
+  const debouncedFetchRestaurants = useCallback(
+    debounce(() => {
+      fetchRestaurants();
+    }, 400),
+    [],
+  );
 
   useEffect(() => {
     fetchRestaurants();
@@ -83,41 +99,48 @@ const RestaurantMap:React.FC<RestaurantMapProps> = ({setClickedRestaurant}) => {
   // pass restaurant info up to parent to use on right section
   const handleMarkerClick = (restaurant: Restaurant) => {
     setClickedRestaurant(restaurant);
-    console.log(restaurant)
+    console.log(restaurant);
   };
 
   // updates the center of the map when it is moved
-  const updateMap = (map:MapEvent) => {
+  const updateMap = (map: MapEvent) => {
     const center = map.detail.center;
     setMapCenter(center);
-  }
+  };
 
   return (
     <>
-    <MapSearch setCurrentLocation={setCurrentLocation}/>
-    <APIProvider apiKey={config.mapsAPIKey}>
-      <div className='w-full h-full'>
-        <Map
-          zoom={17}
-          center={currentLocation}
-          mapId={config.mapID}
-          onCenterChanged={(map) => updateMap(map)}
-        >
-          {restaurants.map((restaurant, index) => (
-            <Marker
-              key={index}
-              position={{lat:restaurant.coordinates.latitude, lng:restaurant.coordinates.longitude}}
-              onClick={() => handleMarkerClick(restaurant)}
-            />
-          ))}
-          <AdvancedMarker position={currentLocation}>
-            <Pin background={'#f472b6'} borderColor={'#fb923c'} glyphColor={'#fb923c'} scale={1.3}/>
-          </AdvancedMarker>
-        </Map>
-      </div>
-    </APIProvider>
+      <MapSearch setCurrentLocation={setCurrentLocation} />
+      <APIProvider apiKey={config.mapsAPIKey}>
+        <div className="w-full h-full">
+          <Map
+            zoom={17}
+            center={currentLocation}
+            mapId={config.mapID}
+            onCenterChanged={(map) => updateMap(map)}
+          >
+            {restaurants.map((restaurant, index) => (
+              <Marker
+                key={index}
+                position={{
+                  lat: restaurant.coordinates.latitude,
+                  lng: restaurant.coordinates.longitude,
+                }}
+                onClick={() => handleMarkerClick(restaurant)}
+              />
+            ))}
+            <AdvancedMarker position={currentLocation}>
+              <Pin
+                background={'#f472b6'}
+                borderColor={'#fb923c'}
+                glyphColor={'#fb923c'}
+                scale={1.3}
+              />
+            </AdvancedMarker>
+          </Map>
+        </div>
+      </APIProvider>
     </>
-
   );
 };
 
