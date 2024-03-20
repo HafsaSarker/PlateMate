9615bbed4263cdfb088129cbef0dd607ced32153
xchen <xchen601@gmail.com>
2024-03-20 05:19:13 -0400

    add upload and retrieve url for backend

--- server/src/controllers/s3Controller.ts
+++ server/src/controllers/s3Controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import AWS from 'aws-sdk';
 import multer from 'multer';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import crypto from 'crypto';
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
@@ -11,6 +13,18 @@ const s3 = new S3Client({
   },
 });
 
+const randomImageName = () => crypto.randomBytes(16).toString('hex');
+
+async function generatePresignedUrl(imageName: string) {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: imageName,
+  };
+  const command = new GetObjectCommand(params);
+  const url = await getSignedUrl(s3, command, { expiresIn: 600000 });
+  return url;
+}
+
 async function uploadImage(req: Request, res: Response) {
   try {
     console.log('req.body:', req.body);
@@ -18,15 +32,17 @@ async function uploadImage(req: Request, res: Response) {
 
     const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: req.file.originalname,
+      Key: randomImageName(),
       Body: req.file.buffer,
       ContentType: req.file.mimetype,
     };
     const command = new PutObjectCommand(params);
-
+    console.log(params.Key)
     await s3.send(command);
 
-    res.send({});
+    const url = await generatePresignedUrl(params.Key);
+    console.log('url:', url);
+    res.send({ imageUrl: url });
 
   } catch (error) {
     res.status(500).json({ error: error.message });
