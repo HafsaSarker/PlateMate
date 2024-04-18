872a2b2266256e00d019b13bc4756a4aa4c73faa
Hafsa Sarker <hafsahaqs@gmail.com>
2024-04-18 17:33:33 -0400

    modify form style

--- client/src/components/map/MapSearch.tsx
+++ client/src/components/map/MapSearch.tsx
@@ -1,11 +1,53 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useContext, useState } from 'react';
 import { MapSearchProps } from '../../types/mapSearchProps';
-import { FaWandMagicSparkles } from 'react-icons/fa6';
+import { UserContext } from '../../context/UserContext';
+import { UserContextType } from '../../types/userContextType';
+
+interface MapSearchForm {
+  restaurantLocation: string;
+  foodCategory: string;
+  restaurantAttributes?: string[];
+  pricePoint?: string[];
+}
 
 const MapSearch: React.FC<MapSearchProps> = ({ showSearch, setShowSearch }) => {
-  const handleChange = () => {};
+  const { currUser, setCurrUser } = useContext(UserContext) as UserContextType;
 
-  const handleSearch = async () => {};
+  const [formData, setFormData] = useState<MapSearchForm>({
+    restaurantLocation: currUser ? currUser.profile.restaurantLocation : '',
+    foodCategory: currUser ? currUser.profile.foodCategory : '',
+    restaurantAttributes: currUser ? currUser.profile.restaurantAttributes : [],
+    pricePoint: currUser ? currUser.profile.pricePoint : [],
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+
+    if (e.target.type === 'select-multiple') {
+      const options = (e.target as HTMLSelectElement).options;
+      const values = Array.from(options)
+        .filter((option) => option.selected)
+        .map((option) => option.value);
+
+      setFormData((prev) => ({
+        ...prev,
+        [name]: values,
+      }));
+    } else {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
 
   return (
     <div
@@ -13,7 +55,7 @@ const MapSearch: React.FC<MapSearchProps> = ({ showSearch, setShowSearch }) => {
       aria-hidden="true"
       className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 backdrop-blur-sm bg-black bg-opacity-20"
     >
-      <div className="relative p-4 w-6/12 max-w-2xl max-h-full">
+      <div className="relative p-4 min-w-fit w-8/12 max-w-2xl max-h-full">
         {/* <!-- Modal content --> */}
         <div className="relative bg-white rounded-lg shadow">
           {/* <!-- Modal header --> */}
@@ -24,126 +66,130 @@ const MapSearch: React.FC<MapSearchProps> = ({ showSearch, setShowSearch }) => {
             <img src="./hearts.png" width={30} alt="" />
           </div>
           {/* <!-- Modal body --> */}
-          <div className="flex flex-col items-center justify-between pt-8 p-4 md:p-5 rounded-t">
-            <form className="flex flex-col gap-2" onSubmit={handleSearch}>
-              <div className="col-span-full">
-                <label
-                  htmlFor="restaurantLocation"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Location Preferred*
-                </label>
-                <div>
-                  <input
-                    type="text"
-                    name="restaurantLocation"
-                    id="restaurantLocation"
-                    // value={formData.restaurantLocation}
-                    className="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                    required
-                    onChange={handleChange}
-                  />
+          <div className="flex flex-col items-center justify-between  rounded-t">
+            <form
+              className="flex flex-col items-center gap-2 w-full"
+              onSubmit={handleSearch}
+            >
+              <div className="p-4 md:p-5">
+                <div className="col-span-full pb-4">
+                  <label
+                    htmlFor="restaurantLocation"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Location Preferred*
+                  </label>
+                  <div>
+                    <input
+                      type="text"
+                      name="restaurantLocation"
+                      id="restaurantLocation"
+                      value={formData.restaurantLocation}
+                      className="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                      required
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-full pb-4">
+                  <label
+                    htmlFor="foodCategory"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Food Category*
+                  </label>
+                  <div>
+                    <input
+                      type="text"
+                      name="foodCategory"
+                      id="foodCategory"
+                      value={formData.foodCategory}
+                      className="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <span className="block text-xs font-medium leading-6 text-gray-900">
+                    View a list of all categories{' '}
+                    <a
+                      href="https://blog.yelp.com/businesses/yelp_category_list/"
+                      target="_blank"
+                      className="text-red-600 hover:underline"
+                    >
+                      here
+                    </a>
+                  </span>
+                </div>
+
+                <div className="col-span-full pb-4">
+                  <label
+                    htmlFor="restaurantAttributes"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Restaurant Attributes
+                  </label>
+
+                  <div>
+                    <select
+                      id="restaurantAttributes"
+                      name="restaurantAttributes"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      multiple
+                      onChange={handleChange}
+                      value={formData.restaurantAttributes}
+                    >
+                      <option value="reservation">Reservation</option>
+                      <option value="deals">Deals</option>
+                      <option value="wheelchair_accessible">
+                        Wheelchair Accessible
+                      </option>
+                      <option value="parking_lot">Parking Lot</option>
+                      <option value="outdoor_seating">Outdoor Seating</option>
+                      <option value="wifi_free">Free Wifi</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="col-span-full pb-4">
+                  <label
+                    htmlFor="pricePoint"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Price Point
+                  </label>
+                  <div>
+                    <select
+                      id="pricePoint"
+                      name="pricePoint"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      multiple
+                      onChange={handleChange}
+                      value={formData.pricePoint}
+                    >
+                      <option value="1">$</option>
+                      <option value="2">$$</option>
+                      <option value="3">$$$</option>
+                      <option value="4">$$$$</option>
+                    </select>
+                  </div>
                 </div>
               </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="foodCategory"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+              {/* <!-- Modal footer --> */}
+              <div className="flex w-full items-center justify-end p-4 md:p-5 border-t border-gray-200 rounded-b">
+                <button
+                  type="submit"
+                  className="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1.5 text-center"
                 >
-                  Food Category*
-                </label>
-                <div>
-                  <input
-                    type="text"
-                    name="foodCategory"
-                    id="foodCategory"
-                    // value={formData.foodCategory}
-                    className="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                    onChange={handleChange}
-                  />
-                </div>
-                <span className="block text-xs font-medium leading-6 text-gray-900">
-                  View a list of all categories{' '}
-                  <a
-                    href="https://blog.yelp.com/businesses/yelp_category_list/"
-                    target="_blank"
-                    className="text-red-600 hover:underline"
-                  >
-                    here
-                  </a>
-                </span>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="restaurantAttributes"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  Search
+                </button>
+                <button
+                  onClick={() => setShowSearch(false)}
+                  className="rounded-lg text-sm px-3 py-1.5  ms-3  font-medium text-gray-900 focus:outline-none bg-white  border border-gray-200 hover:bg-gray-100 hover:text-indigo-600 focus:z-10 focus:ring-4 focus:ring-gray-100"
                 >
-                  Restaurant Attributes
-                </label>
-
-                <div>
-                  <select
-                    id="restaurantAttributes"
-                    name="restaurantAttributes"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    multiple
-                    onChange={handleChange}
-                    // value={formData.restaurantAttributes}
-                  >
-                    <option value="reservation">Reservation</option>
-                    <option value="deals">Deals</option>
-                    <option value="wheelchair_accessible">
-                      Wheelchair Accessible
-                    </option>
-                    <option value="parking_lot">Parking Lot</option>
-                    <option value="outdoor_seating">Outdoor Seating</option>
-                    <option value="wifi_free">Free Wifi</option>
-                  </select>
-                </div>
-              </div>
-
-              <div className="col-span-full">
-                <label
-                  htmlFor="pricePoint"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Price Point
-                </label>
-                <div>
-                  <select
-                    id="pricePoint"
-                    name="pricePoint"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    multiple
-                    onChange={handleChange}
-                    // value={formData.pricePoint}
-                  >
-                    <option value="1">$</option>
-                    <option value="2">$$</option>
-                    <option value="3">$$$</option>
-                    <option value="4">$$$$</option>
-                  </select>
-                </div>
+                  Go Back
+                </button>
               </div>
             </form>
-          </div>
-
-          {/* <!-- Modal footer --> */}
-          <div className="flex items-center justify-end p-4 md:p-5 border-t border-gray-200 rounded-b ">
-            <button
-              type="button"
-              className="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-1.5 text-center"
-            >
-              Search
-            </button>
-            <button
-              onClick={() => setShowSearch(false)}
-              className="rounded-lg text-sm px-3 py-1.5  ms-3  font-medium text-gray-900 focus:outline-none bg-white  border border-gray-200 hover:bg-gray-100 hover:text-indigo-600 focus:z-10 focus:ring-4 focus:ring-gray-100"
-            >
-              Go Back
-            </button>
           </div>
         </div>
       </div>
